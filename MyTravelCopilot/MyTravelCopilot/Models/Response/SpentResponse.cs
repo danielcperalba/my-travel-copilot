64550e967ee57158f7e21efe22e5afe816dd5fd2
Daniel Peralba <contatodanielperalba@hotmail.com>
2024-02-27 21:16:19 -0300

    criando objetos de request e response de gastos

---
+++ MyTravelCopilot/MyTravelCopilot/Models/Response/SpentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MyTravelCopilot.Models.Response
+{
+    public class SpentResponse
+    {
+        public Guid SpentId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double Cost { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
